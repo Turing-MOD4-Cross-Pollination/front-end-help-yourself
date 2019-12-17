09d7ca765715918b344772244e72535c9a4a9a9b
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-12-16 18:01:39 -0700

    Add Home screen buttons and styling

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import HomeScreen from './HomeScreen'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './HomeScreen';
+import ForMeScreen from './ForMeScreen';
+import NearMeScreen from './NearMeScreen';
+import NowScreen from './NowScreen';
+
 const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  // Profile: {screen: ProfileScreen},
+	Home: { screen: HomeScreen },
+	ForMe: { screen: ForMeScreen },
+	NearMe: { screen: NearMeScreen },
+	Now: { screen: NowScreen }
 });
 
 const App = createAppContainer(MainNavigator);
 
-export default App
-
-
-
-
-
-
-
+export default App;
---
+++ ForMeScreen.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const ForMeScreen = () => {
+	const { navigate } = this.props.navigation;
+	return (
+		<View style={styles.container}>
+			<Text>Near Me</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	button: {
+		height: '40%',
+		width: '10%',
+		backgroundColor: 'red'
+	}
+});
+
+export default ForMeScreen;
