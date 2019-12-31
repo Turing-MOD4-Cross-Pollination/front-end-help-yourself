c088d2f3143aa3eff911d9a87b5c4e456cb3cf44
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-30 17:52:27 -0700

    Add api call for all resources

---
+++ util/apiCalls.js
@@ -0,0 +1,16 @@
+// const baseUrl = `${process.env.REACT_NATIVE_BACK_END_URL}`
+const baseUrl = `http://10.1.3.61:4000/resources`;
+
+export const getAllData = () => {
+  const query = `query {resources { id, name, website,street, city, state, zip_code, contact, notes, category, subcategory }}`;
+  
+  const opts = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query })
+  };
+  console.log('BASE===>',`${baseUrl}`)
+  return fetch(`${baseUrl}`, opts)
+    .then(res => res.json())
+    .catch(error=>console.log(error));
+};
