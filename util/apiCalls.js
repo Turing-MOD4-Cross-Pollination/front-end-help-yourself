72dd9da7bccf581aae22cb292bf486189c89cb5c
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-31 00:48:37 -0700

    Alters the IP for current local host

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -1,5 +1,6 @@
 // const baseUrl = `${process.env.REACT_NATIVE_BACK_END_URL}`
-const baseUrl = `http://10.1.3.61:4000/resources`;
+//must be set to local machines ip address when using running the graphql server locally
+const baseUrl = `http://192.168.0.239:4000/resources`;
 
 export const getAllData = () => {
   const query = `query {resources { id, name, website,street, city, state, zip_code, contact, notes, category, subcategory }}`;
