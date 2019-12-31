61c107fb8c9031f2dbf464f8b88cefa0efd7b222
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-31 00:51:21 -0700

    Set the base URL to localhost:4000/resources

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -1,7 +1,7 @@
 // const baseUrl = `${process.env.REACT_NATIVE_BACK_END_URL}`
 //must be set to local machines ip address when using running the graphql server locally
-const baseUrl = `http://192.168.0.239:4000/resources`;
-
+// const baseUrl = `http://192.168.0.239:4000/resources`;
+const baseUrl = 'http://localhost:4000/resources'
 export const getAllData = () => {
   const query = `query {resources { id, name, website,street, city, state, zip_code, contact, notes, category, subcategory }}`;
   
