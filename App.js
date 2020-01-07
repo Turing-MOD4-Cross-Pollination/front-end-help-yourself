682228e4c2bc724065768d2a6e4dced176d2740e
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-07 11:55:30 -0700

    Implement redux-persist

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 // import { createStore, applyMiddleware } from 'redux';
 // import rootReducer from './reducers';
 import Home from './Home';
 // import logger from 'redux-logger'
 // import {getStore} from './store';
-import {configureStore} from './store';
+import { store, persistor } from './store';
 // const store = createStore(rootReducer, applyMiddleware(logger));
 
 const App = () => {
-  // const store = getStore()
-  const { persistor, store} = configureStore()
+
     return (
       <Provider store={store}>
-        <Home />
+        <PersistGate loading={null} persistor={persistor}>
+          <Home />
+        </PersistGate>
       </Provider>
     );
 }
--- store.js
+++ store.js
@@ -1,33 +1,35 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer, persistCombineReducers } from "redux-persist";
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from 'react-native';
 import rootReducer from './reducers'
 import createSecureStore from "redux-persist-expo-securestore";
 // import storeConstants from './constants'
+import logger from 'redux-logger'
+import storage from 'redux-persist/lib/storage'
+// const storage = createSecureStore();
 
-// const store = createStore(rootReducer, storeConstants.DEF_STORE); 
-
-const storage = createSecureStore();
- 
 const config = {
   key: "root",
   storage
 };
- 
-const reducer = persistCombineReducers(config, rootReducer);
- 
-function configureStore() {
 
-  const store = createStore(reducer);
-  const persistor = persistStore(store);
- 
-  return { persistor, store };
-}
+const reducer = persistReducer(config, rootReducer);
 
-// const getStore = () => store;
-// const getState = () => {
-//     return store.getState();
-// };
-// export { getStore, getState };
-export { configureStore };
-export default { getStore, getState,}
+
+export const store = createStore(reducer)
+export const persistor = persistStore(store)
+
+
+
+
+
+// function configureStore() {
+
+//   const store = createStore(reducer);
+//   const persistor = persistStore(store);
+ 
+//   return { persistor, store };
+// }
+
+
+// export { configureStore };
