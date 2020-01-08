ecbabc81c90bd40cc3cf3d78ababc470c6d17cce
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-08 16:09:20 -0700

    Initialize fetch and redux for revocery meetings

--- reducers/index.js
+++ reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import selectedCategories from './selectedCategories';
 import allResources from './allResources';
 import allCategories from './allCategories';
+import allMeetings from './allMeetings';
 
 const rootReducer = combineReducers({
-	allResources,
-	allCategories,
-	selectedCategories
+  allResources,
+  allCategories,
+  selectedCategories,
+  allMeetings,
 });
 
 export default rootReducer;
